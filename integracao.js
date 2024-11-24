247b8c14a1f8e8b76ca5f84102b9f1d38fa44344
Diego Brito <dibbrito@gmail.com>
2024-11-23 23:25:00 -0300

    add api url in vercel

--- integracao.js
+++ integracao.js
@@ -11,8 +11,9 @@ btnSubmit.addEventListener('click', async () => {
         stocks: stockActions, // Enviar todas as ações da lista
     };
 
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     try {
-        const response = await fetch('http://stock-route-brianzav-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/api/v1/stock', {
+        const response = await fetch(`${apiUrl}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
